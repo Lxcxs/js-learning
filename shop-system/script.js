6e7703b42712ba1b76b098a286adfb9918ac2101
Lucas Cupertino <olucas.cdev@gmail.com>
2022-07-19 01:16:33 -0300

    button hover add

--- shop-system/script.js
+++ shop-system/script.js
@@ -1,43 +1,97 @@
 const reviews = [
     {
-        nome: 'Produto',
-        img: './tenis.png'
+        nome: 'Tênis Air Jordan 1 Mid SE Masculino',
+        img: './tenis.png',
+        preco: 1099.99
     },
     {
-        nome: 'Produto',
-        img: './tenis.png'
+        nome: 'Tênis Air Jordan 1 Mid SE Masculino',
+        img: './tenis.png',
+        preco: 1099.99
     },
     {
-        nome: 'Produto',
-        img: './tenis.png'
+        nome: 'Tênis Air Jordan 1 Mid SE Masculino',
+        img: './tenis.png',
+        preco: 1099.99
     },
     {
-        nome: 'Produto',
-        img: './tenis.png'
+        nome: 'Tênis Air Jordan 1 Mid SE Masculino',
+        img: './tenis.png',
+        preco: 1099.99
     },
     {
-        nome: 'Produto',
-        img: './tenis.png'
+        nome: 'Tênis Air Jordan 1 Mid SE Masculino',
+        img: './tenis.png',
+        preco: 1099.99
     },
     {
-        nome: 'Produto',
-        img: './tenis.png'
+        nome: 'Tênis Air Jordan 1 Mid SE Masculino',
+        img: './tenis.png',
+        preco: 1099.99
+    },
+    {
+        nome: 'Tênis Air Jordan 1 Mid SE Masculino',
+        img: './tenis.png',
+        preco: 1099.99
+    },
+    {
+        nome: 'Tênis Air Jordan 1 Mid SE Masculino',
+        img: './tenis.png',
+        preco: 1099.99
+    },
+    {   
+        nome: 'Tênis Air Jordan 1 Mid SE Masculino',
+        img: './tenis.png',
+        preco: 1099.99  
+    },
+    {
+        nome: 'Tênis Air Jordan 1 Mid SE Masculino',
+        img: './tenis.png',
+        preco: 1099.99
     }
 ]
 
+function initOpen() {
 
-for (let i = 0; i < reviews.length; i++) {
-    setTimeout(() => {
+    for (let i = 0; i < reviews.length; i++) {
 
-        const div = document.createElement('div');
-        let texto = document.createElement('h1');
-        let image = document.createElement('img')
+            const div = document.createElement('div');
+            const h1 = document.createElement('h1');
+            const h2 = document.createElement('h2');
+            const image = document.createElement('img');
 
-        document.querySelector('.container').appendChild(div).classList.add('box');
-        div.appendChild(texto).innerHTML = `${reviews[i].nome} ${i+1}`;
-        image.src = reviews[i].img;
-        div.appendChild(image)
+            document.querySelector('.container').appendChild(div).classList.add('box');
+            div.appendChild(h2).innerHTML = `R$ ${reviews[i].preco}`;
+            div.appendChild(h1).innerHTML = `${reviews[i].nome}`;
+            image.src = reviews[i].img;
+            div.appendChild(image);
 
-    }, 1000 * i)
+    }
+
+
 
 }
+
+initOpen();
+
+const produtos = document.querySelectorAll('.box');
+
+produtos.forEach(prod => {
+    const div2 = document.createElement('div');
+    const link = document.createElement('a');
+
+    link.innerHTML = 'Adicionar';
+    link.href = '#';
+
+
+    div2.appendChild(link);
+    prod.appendChild(div2).classList.add('addToCart');
+
+
+    prod.addEventListener('mouseover', () => {
+        div2.classList.add('active')
+    })
+    prod.addEventListener('mouseout', () => {
+        div2.classList.remove('active')
+    })
+})
