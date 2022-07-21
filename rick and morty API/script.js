a1fadc1dfb8da880e4047120eedd2c564e62a1f1
Lucas Cupertino <olucas.cdev@gmail.com>
2022-07-21 16:55:58 -0300

    commit

--- rick and morty API/script.js
+++ rick and morty API/script.js
@@ -4,24 +4,18 @@ async function pegaDados() {
 
     const container = document.querySelector('.container');
 
-    async function teste() {
-        for (i in characters.results) { 
-            console.log(i)
-            const div = document.createElement('div');
-            const h1 = document.createElement('h1');
-            const h3 = document.createElement('h3');
+    for (i in characters.results) { 
+        console.log(i)
+        const div = document.createElement('div');
+        const h1 = document.createElement('h1');
+        const h3 = document.createElement('h3');
 
-            container.appendChild(div);
-            div.appendChild(h1);
-            div.appendChild(h3);
-            h1.innerHTML = characters.results[i].name;
-            h3.innerHTML = characters.results[i].status;
-
-        }
+        container.appendChild(div);
+        div.appendChild(h1);
+        div.appendChild(h3);
+        h1.innerHTML = characters.results[i].name;
+        h3.innerHTML = characters.results[i].status;
     }
-    teste()
-
-
 }
 
 pegaDados()
